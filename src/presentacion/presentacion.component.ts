697d7e45e50dd515e68e560c72daf8ed70d90529
Theshy520-spec <q1314xfq@gmail.com>
2025-09-30 11:53:45 +0200

    .

---
+++ src/presentacion/presentacion.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentacion',
+  templateUrl: './presentacion.component.html',
+  styleUrl: './presentacion.component.css'
+})
+export class PresentacionComponent {
+}
